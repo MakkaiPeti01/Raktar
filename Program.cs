edc1412c007f5e91cf40e6bc71c06a66c5f84b10
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-23 14:55:03 +0200

    beolvas

--- Program.cs
+++ Program.cs
@@ -9,10 +9,21 @@ namespace Raktar
 {
     class Program
     {
+        static void BeolvasRaktar()
+        {
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                string[] sor = raktar.ReadLine().Split(';');
+                
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
-            Termek t = new Termek("P0", "Polo XXL", 1500, 10);
-            Console.WriteLine($"{t.KOD} - {t.NEV} - {t.AR} - {t.DB}");
+            //Termek t = new Termek("P0", "Polo XXL", 1500, 10);
+            //Console.WriteLine($"{t.KOD} - {t.NEV} - {t.AR} - {t.DB}");
+            BeolvasRaktar();
             Console.ReadKey();
         }
     }
