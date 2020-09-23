c1f49aebf25308939531e753ef5cda17f8ebd13e
MakkaiPeti01 <PetyaPower@gmail.com>
2020-09-23 14:47:01 +0200

    osztaly letrehozasa

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace Raktar
     {
         static void Main(string[] args)
         {
+            Termek t = new Termek("P0", "Polo XXL", 1500, 10);
+            Console.WriteLine($"{t.KOD} - {t.NEV} - {t.AR} - {t.DB}");
             Console.ReadKey();
         }
     }
